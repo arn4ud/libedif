b81cea4f59f7537642672b3e98fe860f5ddce629
Arnaud Caron <arn4ud@gmail.com>
2022-12-05 16:12:06 -0800

    Add some tests

--- src/edif/edif_reader_test.cpp
+++ src/edif/edif_reader_test.cpp
@@ -1,12 +1,20 @@
-#include "edif_reader/edif_reader.h"
+#include "edif/edif_reader.h"
 #include "gtest/gtest.h"
 
 namespace {
 
-TEST(EdifReaderTest, test0) {
+TEST(EdifReaderTest, des) {
     edif::EdifReader reader;
     reader.read("testdata/des.edf");
+}
+
+TEST(EdifReaderTest, fpu) {
+    edif::EdifReader reader;
     reader.read("testdata/fpu.edf");
+}
+
+TEST(EdifReaderTest, or1200) {
+    edif::EdifReader reader;
     reader.read("testdata/or1200.edf");
 }
 
